f23c9089abbaa890ab451b96ab822e9b8e0c229b
JC Yan <ahhorsegan@gmail.com>
2022-11-07 16:28:26 -0800

    Fixed null error and fixed star image paths

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -41,10 +41,12 @@ function addRecipesToDocument(recipes) {
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
-  for(let i = 0; i < recipes.length; i++){
-      let recipeCard = document.createElement('recipe-card');
-      recipeCard.data = recipes[i];
-      mainElement.append(recipeCard);
+  if(recipes != null) {
+    for(let i = 0; i < recipes.length; i++){
+        let recipeCard = document.createElement('recipe-card');
+        recipeCard.data = recipes[i];
+        mainElement.append(recipeCard);
+    }
   }
 }
 
